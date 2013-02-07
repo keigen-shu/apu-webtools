0d8902c59f14524eb91761d38482618894a3bbb0
Keigen Shu <keigen.shu@gmail.com>
2013-02-08 00:49:03 +0800

    Added intake listing and week tracking.

--- apu_webtools/app.py
+++ apu_webtools/app.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 from flask import Flask, render_template, request, url_for
-from apu_webtools.timetable import get_table
+from apu_webtools.timetable import get_timetable, get_timetable_intake_list
 
 app = Flask(__name__)
 """
@@ -10,7 +10,16 @@ def root():
     return render_template('timetable.html', entries=table['entries'], modified=table['last_modified'])
 """
 
+@app.route("/timetable")
+def timetable_list():
+    result = get_timetable_intake_list()
+    return render_template('timetable_intakes.html', groups=result['groups'], intake_lists=result['intake_lists'])
+
+@app.route("/timetable/<intake>/<week>")
+def timetable(intake, week):
+    result = get_timetable(intake, int(week))
+    return render_template('timetable.html', intake=result['intake'], table=result['table'], modified=result['last_modified'], week=int(week))
+
 @app.route("/timetable/<intake>")
-def timetable(intake):
-    result = get_table(intake)
-    return render_template('timetable.html', intake=result['intake'], table=result['table'], modified=result['last_modified'])
+def timetable_now(intake):
+    return timetable(intake, 0);
--- apu_webtools/timetable.py
+++ apu_webtools/timetable.py
@@ -8,7 +8,49 @@ import datetime
 import re
 import urllib
 
-def get_table(intake, week_inc=0, filters=None):
+def get_timetable_intake_list():
+    # send request and get page
+    try:
+        response = urllib.urlopen('http://webspace.apiit.edu.my/intake-timetable/')
+    except timeout:
+        exit('Error: URL request timeout')
+
+    page = response.read()
+
+    s = page.find('<script language="javascript" type="text/javascript">')
+    e = page.find('//', s)
+
+    if s == -1 or e == -1:
+        exit("Error: could not find page pattern.")
+
+    s = page[s:e].find('data')
+
+    section = page[s:e]
+
+    # extract and print data
+    matches = re.findall(r"'([A-Z0-9\{\}]+)'", section)
+
+    if len(matches) == 0:
+        exit('Host intake list is empty')
+
+    groups  = [  'UCDF / UCFF',         'UC1F',    'UC2F',    'UC3F',    'UC4F',    'UCM / UCP',          'Others']
+    regexes = [ r'(UCD[12]?F)|(UCFF)', r'(UC1F)', r'(UC2F)', r'(UC3F)', r'(UC4F)', r'(UCM)|(UC[123]?P)', r'.+' ]
+
+    intake_lists = []
+
+    for i in range(len(groups)):
+        intake_lists.append([])
+
+    for intake in sorted(matches):
+        for i in range(len(groups)):
+            if (re.match(regexes[i], intake[:5])):
+                intake_lists[i].append(intake)
+                break
+
+    return { 'groups': groups, 'intake_lists': intake_lists }
+
+
+def get_timetable(intake, week_inc=0, filters=None):
     # find out the Monday date of the week we want
     week = datetime.date.today()
     week = week + datetime.timedelta(days=-week.weekday(), weeks=week_inc)
@@ -39,26 +81,31 @@ def get_table(intake, week_inc=0, filters=None):
     # extract and print data
     matches = re.findall(r'(<tr> (?:<td> [\s\w:.,\-\/]+ </td> ){6}?</tr>)', section)
     modified = re.findall(r'Last modified: ([\s\w:]+)</p>', page[e:e+128])
+    
+    if len(modified) == 0:
+        modified = None
+    else:
+        modified = modified[0].strip()
+    
+    last_date = str()
+    table     = str()
 
     if len(matches) == 0:
-        print("there are no classes")
+        table = None
+    else: 
+        for entry in matches:
+            fields = re.findall(r'(?:<td> ([\s\w:.,\-\/]+) </td> )', entry)
 
-    last_date = str()
-    
-    table = str()
-    for entry in matches:
-        fields = re.findall(r'(?:<td> ([\s\w:.,\-\/]+) </td> )', entry)
+            # filter entries
+            if (last_date != fields[0]):
+                table += '<tr class="section"><td colspan="4">' + fields[0] + '</td></tr>'
+                last_date = fields[0]
 
-        # filter entries
-        if (last_date != fields[0]):
-            table += '<tr class="section"><td colspan="4">' + fields[0] + '</td></tr>'
-            last_date = fields[0]
+            table += '<tr><td>' + fields[1] + '</td><td>' + fields[3] + ' : ' + fields[2] + '</td><td>' + fields[4] + '</td><td>' + fields[5] +'</td></tr>'
 
-        table += '<tr><td>' + fields[1] + '</td><td>' + fields[3] + ' : ' + fields[2] + '</td><td>' + fields[4] + '</td><td>' + fields[5] +'</td></tr>'
-        
     return {'intake': intake,
             'week': week.isoformat(),
-            'last_modified': modified[0].strip(),
+            'last_modified': modified,
             'table': table
             }
 
